68941669d1722ca91841f322925b378774d1b2df
Тонхоноев Александр Андреевич <a.a.tonkhonoev@novotelecom.ru>
2019-10-29 15:06:31 +0700

    Initial Commit

---
+++ Wheather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wheather
+//
+//  Created by Александр Тонхоноев on 29.10.2019.
+//  Copyright © 2019 Александр Тонхоноев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
